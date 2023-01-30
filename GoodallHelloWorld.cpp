47ac1311874d9731b52f1e23b8d9e26c2817247c
Melissa Goodall <ms.melissa.goodall@gmail.com>
2023-01-30 14:27:16 -0500

    Edited whitespace for better printing

--- GoodallHelloWorld.cpp
+++ GoodallHelloWorld.cpp
@@ -1,19 +1,16 @@
-// Your First C++ Program                                               //this is a standard header comment
+// Your First C++ Program               //this is a standard header comment
 // Coded by: Melissa Goodall
 // Date:  1/30/2023
-// Version 1.0                                                          //this is the baseline version. Minor changes go up in decimal. major in integer
+// Version 1.0                          //this is the baseline version. Minor changes go up in decimal. major in integer
 
-#include <iostream>                                                     //libraries for input from keyboard and output to screen
+#include <iostream>                     //libraries for input from keyboard and output to screen
 
 int main() {
-    std::cout << "Hello World!";                                        //print out to the stream. command cout is part of the standard (std) library namespace 
-    return 0;                                                           // returning integer 0 indicates program ran successfully and closes program
-}                                                                       //note:  The debugger will stay open, but a release version of the code
-                                                                        //will not!  Change your VS Preferences to always close debugger to emulate
-                                                                        //real world
+    std::cout << "Hello World!";        //print out to the stream. command cout is part of the standard (std) library namespace 
+    return 0;                           // returning integer 0 indicates program ran successfully and closes program
 
-                                                                        //enable Tools->Options->Debugging->Automatically close the console when debugging stops
+ }                                      //note:  The debugger will stay open, but a release version of the code
+                                        //will not!  Change your VS Preferences to always close debugger to emulate
+                                        //real world
 
-//demonstrate poor structure without lines and spaces
-//demonstrate difference of using namespace without using name space
-//demonstrate common syntax errors
+                                        //enable Tools->Options->Debugging->Automatically close the console when debugging stops
